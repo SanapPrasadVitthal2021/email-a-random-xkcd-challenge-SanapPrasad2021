358506e7523aefb97c639f46a90fa3f4275b6689
SanapPrasad2021 <95181420+SanapPrasad2021@users.noreply.github.com>
2021-12-26 20:07:47 +0530

    main

--- sendMail.php
+++ sendMail.php
@@ -5,7 +5,7 @@
     {
         $to=$email;
         $subject="Email Verification";
-        $message= "<h3>Hey $fname $lname, you're almost ready to start enjoing<strong> XKCD Comics</strong>.Simply verify your email address with below.</h3><br><h3>Verification OTP:<br>$vkey</h3><br><br><br><h5>Thank you.</h5>";
+        $message= "<h3>Hey $fname $lname, you're almost ready to start enjoing<strong> XKCD Comics</strong>.Simply verify your email address with your one time password.</h3><br><h3>Verification OTP:<br>$vkey</h3><br><br><br><h5>Thank you.</h5>";
         $sender = "From: prasadsanap@epavitram.com\r\n";
         $sender .= "MIME-Version: 1.0"."\r\n";
         $sender .="Content-type:text/html;charset=UTF-8"."\r\n";
