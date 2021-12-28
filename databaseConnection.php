d2e7554e9c183975b94a8026e612daef06eefab7
SanapPrasad2021 <95181420+SanapPrasad2021@users.noreply.github.com>
2021-12-28 17:52:49 +0530

    main

--- databaseConnection.php
+++ databaseConnection.php
@@ -1,9 +1,9 @@
 <?php
     
-    $username=//Enter here your user name.
-    $password=//Enter password here
-    $database=//Enter database name here
-    
+    $username=sampleUsername;//Enter here your user name.
+    $password=samplePassword;//Enter password here
+    $database=sampleDatabase;//Enter database name here
+
     $mysqli=NEW MySQLi('localhost',$username,$password,$database);
 
 ?>
